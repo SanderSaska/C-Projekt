9acb67fa23d2f7be667c64501c6fdc5aa3556172
SanderSaska <sander.saska@gmail.com>
2023-05-19 14:44:38 +0300

    --! Lisad !-- * Avaaken tehtud * Välju nupp töötab * Mängi nupp hetkel mitte (pole sisu, kuhu minna)

--- GUI/lib/mainwindow.h
+++ GUI/lib/mainwindow.h
@@ -3,19 +3,25 @@
 
 #include <QMainWindow>
 
-QT_BEGIN_NAMESPACE
-namespace Ui { class MainWindow; }
-QT_END_NAMESPACE
+namespace Ui {
+class MainWindow;
+}
 
 class MainWindow : public QMainWindow
 {
     Q_OBJECT
 
 public:
-    MainWindow(QWidget *parent = nullptr);
+    explicit MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
+
+private slots:
+
+
+    void on_pushButton_2_clicked();
 
 private:
     Ui::MainWindow *ui;
 };
+
 #endif // MAINWINDOW_H
--- GUI/src/mainwindow.cpp
+++ GUI/src/mainwindow.cpp
@@ -1,15 +1,24 @@
 #include "../lib/mainwindow.h"
-#include "./ui_mainwindow.h"
+#include "ui_mainwindow.h"
 
-MainWindow::MainWindow(QWidget *parent)
-    : QMainWindow(parent)
-    , ui(new Ui::MainWindow)
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
 {
+
+    setFixedSize(780, 320);
+
     ui->setupUi(this);
+
+    //ui->textBrowser->setTextInteractionFlags(Qt::NoTextInteraction);
+    //ui->textBrowser_2->setTextInteractionFlags(Qt::NoTextInteraction);
+
+    QObject::connect(ui->actionV_lju, &QAction::triggered, this, &MainWindow::close);
+
+    QObject::connect(ui->pushButton_2, &QPushButton::clicked, this, &MainWindow::close);
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
 }
-
